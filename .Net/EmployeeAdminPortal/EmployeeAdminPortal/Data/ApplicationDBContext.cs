ba846c54a9a6b421c844f987662e60673c70fa9f
Dendy SA <dendysaptoadi160@gmail.com>
2025-04-27 17:17:14 +0700

    Application update

--- .Net/EmployeeAdminPortal/EmployeeAdminPortal/Data/ApplicationDBContext.cs
+++ .Net/EmployeeAdminPortal/EmployeeAdminPortal/Data/ApplicationDBContext.cs
@@ -1,6 +1,30 @@
-﻿namespace EmployeeAdminPortal.Dat
+﻿using EmployeeAdminPortal.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAdminPortal.Dat
 {
-    public class ApplicationDBContext
+    public class ApplicationDBContext: DbContext
     {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; } = null!;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.Email)
+                .IsRequired()
+                .HasMaxLength(100);
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.Phone)
+                .HasMaxLength(15);
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.Position)
+                .HasMaxLength(50);
+        }
     }
 }
