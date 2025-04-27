bfac75dbb36390b8b0410e747d85c8be5d48e54e
Dendy SA <dendysaptoadi160@gmail.com>
2025-04-27 13:52:44 +0700

    Employee Admin

---
+++ .Net/EmployeeAdminPortal/EmployeeAdminPortal/Data/ApplicationDBContext.cs
@@ -0,0 +1,6 @@
+﻿namespace EmployeeAdminPortal.Dat
+{
+    public class ApplicationDBContext
+    {
+    }
+}
---
+++ .Net/EmployeeAdminPortal/EmployeeAdminPortal/Models/Entities/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeeAdminPortal.Models.Entities
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Position { get; set; }
+        public decimal Salary { get; set; }
+
+    }
+}
