4ba8d59982daedba4e64c9e197dcd3077ffce12c
Dendy SA <dendysaptoadi160@gmail.com>
2025-05-01 16:24:08 +0700

    begineer

--- html/javascript-course-main/2-copy-of-code/lesson-13/scripts/amazon.js
+++ html/javascript-course-main/2-copy-of-code/lesson-13/scripts/amazon.js
@@ -56,6 +56,111 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+const filterData = () => {
+  const valueSearch = document.getElementById('search-bar').value
+  
+  const filteredProducts = products.filter((data) => 
+    new RegExp(valueSearch, 'gi').test(data.name)
+  );
+
+  console.log(filteredProducts)
+  
+  let newProductsHTML = ''
+
+  filteredProducts.forEach((product) => {
+    newProductsHTML += `
+  <div class="product-container">
+    <div class="product-image-container">
+      <img class="product-image"
+        src="${product.image}">
+    </div>
+
+    <div class="product-name limit-text-to-2-lines">
+      ${product.name}
+    </div>
+
+    <div class="product-rating-container">
+      <img class="product-rating-stars"
+        src="images/ratings/rating-${product.rating.stars * 10}.png">
+      <div class="product-rating-count link-primary">
+        ${product.rating.count}
+      </div>
+    </div>
+
+    <div class="product-price">
+      $${(product.priceCents / 100).toFixed(2)}
+    </div>
+
+    <div class="product-quantity-container">
+      <select>
+        <option selected value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+      </select>
+    </div>
+
+    <div class="product-spacer"></div>
+
+    <div class="added-to-cart">
+      <img src="images/icons/checkmark.png">
+      Added
+    </div>
+
+    <button class="add-to-cart-button button-primary js-add-to-cart"
+    data-product-id="${product.id}">
+      Add to Cart
+    </button>
+  </div>
+`;
+});
+
+document.querySelector('.js-products-grid').innerHTML = newProductsHTML;
+
+}
+
+
+const APICallback = async (url, successFunction, failedFunction) => {
+  console.log(var1);
+
+  const getData = fetch(url)
+
+  if (getData.status === 'berhasil') {
+  successFunction()
+} else {
+  failedFunction()
+}
+}
+
+const errorHandling = () => {
+  console.log('error')
+}
+
+APICallback('listProduct', () => filterData(), errorHandling)
+// testCallback('Ini terpanggil', () => {
+//   console.log('ini kalo terpanggil callback berhasil');
+// })
+
+
+
+document.getElementById('search-bar').addEventListener('keypress',
+  (e) => {
+    if (e.key === 'Enter') {
+      filterData()
+    }
+  }
+)
+
+document.getElementById('search-button').addEventListener('click',
+  filterData
+)
+
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
